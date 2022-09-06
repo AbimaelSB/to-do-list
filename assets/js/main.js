bf57861a0a70b844f23f7f66198584eef7fad9b3
AbimaelSB <abimael.barros@yahoo.com>
2022-09-06 02:51:38 -0300

    upload files

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,14 +4,14 @@ var tarefas = [];
 var lista = document.getElementById('to-do');
 
 function novaTarefa() {
-  if(tarefa.value.length < 4  || tarefa.value.length > 50) {
+  if(tarefa.value.length == 0) {
+    alert("Insira a tarefa a ser realizada!")
+  }else if(tarefa.value.length < 4  || tarefa.value.length > 50) {
     alert("Insira entre 4 e 50 caracteres!")
-  }else if(tarefa.value != "") {
+  }else{
     tarefas.push('<p><input type="checkbox">' + tarefa.value + '</p>');
     lista.innerHTML = tarefas.join('');
     tarefa.value = "";
-  }else{
-    alert("Insira a tarefa a ser realizada!")
   }
 }
 
