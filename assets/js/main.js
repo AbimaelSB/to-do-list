bafa720da367886837d8ec510452352ebf211f73
AbimaelSB <abimael.barros@yahoo.com>
2022-09-06 16:39:49 -0300

    upload files

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,7 @@ var addTarefa = document.getElementById('add');
 var tarefa = document.getElementById('tarefa');
 var tarefas = [];
 var lista = document.getElementById('to-do');
+var task = document.getElementsByTagName('p');
 
 function novaTarefa() {
   if(tarefa.value.length == 0) {
@@ -9,7 +10,8 @@ function novaTarefa() {
   }else if(tarefa.value.length < 4  || tarefa.value.length > 50) {
     alert("Insira entre 4 e 50 caracteres!")
   }else{
-    tarefas.push('<p><input type="checkbox">' + tarefa.value + '</p>');
+    let task = '<p><input type="checkbox">' + tarefa.value + '</p>'
+    tarefas.push(task);
     lista.innerHTML = tarefas.join('');
     tarefa.value = "";
   }
